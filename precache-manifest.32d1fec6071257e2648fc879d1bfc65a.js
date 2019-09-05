e7c024128ee98b1fd433f90fd742c6a17d2a9a5a
Adm1ralDen <games.zadrotina@gmail.com>
2019-09-06 00:29:21 +0300

    Updates

---
+++ precache-manifest.32d1fec6071257e2648fc879d1bfc65a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467cf0b44a8291acd33065e6fb4cd2e1",
+    "url": "/pokedex-v5/index.html"
+  },
+  {
+    "revision": "f03f302472c1c6412abf",
+    "url": "/pokedex-v5/static/css/2.848db5fd.chunk.css"
+  },
+  {
+    "revision": "1590bd2465d3739faab2",
+    "url": "/pokedex-v5/static/css/main.91bbfe04.chunk.css"
+  },
+  {
+    "revision": "f03f302472c1c6412abf",
+    "url": "/pokedex-v5/static/js/2.a3ce305b.chunk.js"
+  },
+  {
+    "revision": "1590bd2465d3739faab2",
+    "url": "/pokedex-v5/static/js/main.de8df160.chunk.js"
+  },
+  {
+    "revision": "ed6f79adaf92012d3111",
+    "url": "/pokedex-v5/static/js/runtime~main.9d4bb4c9.js"
+  }
+]);
